161ac458bfc9593dba625ceaee1b3e05b0daebb0
mabindajoash <mabindajoash@gmail.com>
2025-05-01 13:33:59 +0000

    task 4 triggers

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- Create a trigger
+-- Statement to create a trigger that decreases quantity after new order
+DELIMITER $$
+
+CREATE TRIGGER buy_items AFTER INSERT ON order
+FOR EACH ROW
+BEGIN
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
+END$$
+DELIMITER ;
